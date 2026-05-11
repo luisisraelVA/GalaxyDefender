92d2fb98c931edb8937e22271308bfa724e11618
luisisraelVA <luisillo5938@gmail.com>
2026-05-10 22:23:59 -0400

    ¨¨

--- Source/GalaxyDefender/GalaxyDefender.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "GalaxyDefender.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, GalaxyDefender, "GalaxyDefender");
-
-DEFINE_LOG_CATEGORY(LogFlying)
